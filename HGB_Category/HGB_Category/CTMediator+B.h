9b7ff94d331556673938e9ca064777b8505cec82
huzhihui <wonderfulhzh@163.com>
2018-07-02 11:06:23 +0800

    1.1

---
+++ HGB_Category/HGB_Category/CTMediator+B.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+B.h
+//  HGB_Category
+//
+//  Created by 胡志辉 on 2018/7/2.
+//  Copyright © 2018年 胡志辉. All rights reserved.
+//
+
+#import "CTMediator.h"
+
+@interface CTMediator (B)
+-(UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+@end
---
+++ readme.md
@@ -0,0 +1,1 @@
+pod "HGB_Category"
